03242554418282d1fb0717212d160b5b97cdca11
kobel <kobelei335@gmail.com>
2024-07-29 13:05:45 -0700

    speed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import AboutSection from "@/components/AboutSection"
 import HeroSection from "@/components/HeroSection"
 import ProjectsSection from "@/components/ProjectsSection"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+        <SpeedInsights/>
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
